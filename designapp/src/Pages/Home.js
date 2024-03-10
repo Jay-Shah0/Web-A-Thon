1672628a093d073ddd642ca2b38b8069e541496a
Jay-Shah0 <147727327+Jay-Shah0@users.noreply.github.com>
2024-03-10 19:09:21 +0530

    Add files via upload

---
+++ designapp/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "../Components/Home/navbar";
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        
+    </div>
+  )
+}
+
+export default Home;
