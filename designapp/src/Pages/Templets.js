a3de1ca854bb1b6dacbc58d47375cb14e75d032c
Jay-Shah0 <147727327+Jay-Shah0@users.noreply.github.com>
2024-03-12 06:36:31 +0530

    Delete designapp directory

--- designapp/src/Pages/Templets.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Templets = () => {
-  return (
-    <div>Templets</div>
-  )
-}
-
-export default Templets
--- designapp/src/Pages/UseAI.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UseAI = () => {
-  return (
-    <div>UseAI</div>
-  )
-}
-
-export default UseAI
